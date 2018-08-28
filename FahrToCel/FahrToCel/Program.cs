dd5e234525c6b503fdba6aae3752d18b8c41e2f1
crazyotto <34284410+crazyotto@users.noreply.github.com>
2018-08-28 14:36:56 -0500

    intial commit

---
+++ FahrToCel/FahrToCel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FahrToCel
+{
+    class Program
+    {
+    
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a temperature in degrees fahrenheit: ");
+            int temperature = Convert.ToInt32(Console.ReadLine());
+            
+
+             
+            double c = (temperature - 32) * 5 / 9;
+            Console.WriteLine(temperature + " degrees fahrenheit is " + c + " degrees celsius");
+            Console.ReadLine();
+                
+            
+
+            
+        }
+
+
+        
+    }
+}
